4f0b9966b2219face603be07b7814651c867c658
VishnuVardhan <VishnuVardhan@Rev-PF3C66CF>
2025-07-14 13:26:25 +0530

    project_base

---
+++ src/main/java/com/skillscreen/SkillScreenApplication.java
@@ -0,0 +1,13 @@
+package com.skillscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillScreenApplication.class, args);
+	}
+
+}
